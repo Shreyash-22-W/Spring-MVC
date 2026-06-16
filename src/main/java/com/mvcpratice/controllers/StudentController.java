12740086ddfe53d4c480dc44d812a5a278ce0a75
Shreyash <shreyashwaghmare022@gmail.com>
2026-06-16 09:31:42 +0530

    New Controller is added

--- src/main/java/com/mvcpratice/controllers/StudentController.java
+++ src/main/java/com/mvcpratice/controllers/StudentController.java
@@ -7,13 +7,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.service.annotation.GetExchange;
 
 @Controller
-public class StudentController
-{
-   // @ResponseBody
+public class StudentController {
+    // @ResponseBody
     @GetMapping("/student/home")
-    public String showsStudentHomePage()
-    {
+    public String showsStudentHomePage() {
         return "home-page";
     }
 
+    @GetMapping("Student/home/v1")
+    public String showsStudentHomePages() {
+        return "home-page";
+
+    }
 }
